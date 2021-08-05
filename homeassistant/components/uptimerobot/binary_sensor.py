a4eddb8e775425ed371b32e38aee9ea0d7313539
errir <errir@outlook.jp>
2021-08-05 00:03:31 +0000

    Add temporary fix to modbus to solve upstream problem (#53857) home-assistant/core b2fffdd13e55afedad774820576fecee3ea2e0a9

--- homeassistant/components/uptimerobot/binary_sensor.py
+++ homeassistant/components/uptimerobot/binary_sensor.py
@@ -1,10 +1,6 @@
 """A platform that to monitor Uptime Robot monitors."""
-from dataclasses import dataclass
-from datetime import timedelta
-import logging
+from __future__ import annotations
 
-import async_timeout
-from pyuptimerobot import UptimeRobot
 import voluptuous as vol
 
 from homeassistant.components.binary_sensor import (
@@ -13,110 +9,61 @@ from homeassistant.components.binary_sensor import (
     BinarySensorEntity,
     BinarySensorEntityDescription,
 )
-from homeassistant.const import ATTR_ATTRIBUTION, CONF_API_KEY
+from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
+from homeassistant.const import CONF_API_KEY
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import PlatformNotReady
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity,
-    DataUpdateCoordinator,
-    UpdateFailed,
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
+from .const import DOMAIN
+from .entity import UptimeRobotEntity
+
+PLATFORM_SCHEMA = cv.deprecated(
+    vol.All(PLATFORM_SCHEMA.extend({vol.Required(CONF_API_KEY): cv.string}))
 )
-
-_LOGGER = logging.getLogger(__name__)
-
-ATTR_TARGET = "target"
-
-ATTRIBUTION = "Data provided by Uptime Robot"
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({vol.Required(CONF_API_KEY): cv.string})
-
-
-@dataclass
-class UptimeRobotBinarySensorEntityDescription(BinarySensorEntityDescription):
-    """Entity description for UptimeRobotBinarySensor."""
-
-    target: str = ""
 
 
 async def async_setup_platform(
-    hass: HomeAssistant, config, async_add_entities, discovery_info=None
-):
-    """Set up the Uptime Robot binary_sensors."""
-    uptime_robot_api = UptimeRobot()
-    api_key = config[CONF_API_KEY]
-
-    async def async_update_data():
-        """Fetch data from API UptimeRobot API."""
-
-        def api_wrapper():
-            return uptime_robot_api.getMonitors(api_key)
-
-        async with async_timeout.timeout(10):
-            monitors = await hass.async_add_executor_job(api_wrapper)
-            if not monitors or monitors.get("stat") != "ok":
-                raise UpdateFailed("Error communicating with Uptime Robot API")
-            return monitors
-
-    coordinator = DataUpdateCoordinator(
-        hass,
-        _LOGGER,
-        name="uptimerobot",
-        update_method=async_update_data,
-        update_interval=timedelta(seconds=60),
+    hass: HomeAssistant,
+    config: ConfigType,
+    async_add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    """Set up the Uptime Robot binary_sensor platform."""
+    hass.async_create_task(
+        hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=config
+        )
     )
 
-    await coordinator.async_refresh()
 
-    if not coordinator.data or coordinator.data.get("stat") != "ok":
-        _LOGGER.error("Error connecting to Uptime Robot")
-        raise PlatformNotReady()
-
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+) -> None:
+    """Set up the Uptime Robot binary_sensors."""
+    coordinator: DataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
     async_add_entities(
         [
             UptimeRobotBinarySensor(
                 coordinator,
-                UptimeRobotBinarySensorEntityDescription(
-                    key=monitor["id"],
-                    name=monitor["friendly_name"],
-                    target=monitor["url"],
+                BinarySensorEntityDescription(
+                    key=str(monitor.id),
+                    name=monitor.name,
                     device_class=DEVICE_CLASS_CONNECTIVITY,
                 ),
+                target=monitor.url,
             )
-            for monitor in coordinator.data["monitors"]
+            for monitor in coordinator.data
         ],
-        True,
     )
 
 
-class UptimeRobotBinarySensor(BinarySensorEntity, CoordinatorEntity):
+class UptimeRobotBinarySensor(UptimeRobotEntity, BinarySensorEntity):
     """Representation of a Uptime Robot binary sensor."""
 
-    def __init__(
-        self,
-        coordinator: DataUpdateCoordinator,
-        description: UptimeRobotBinarySensorEntityDescription,
-    ) -> None:
-        """Initialize Uptime Robot the binary sensor."""
-        super().__init__(coordinator)
-        self.coordinator = coordinator
-        self.entity_description = description
-        self._attr_extra_state_attributes = {
-            ATTR_ATTRIBUTION: ATTRIBUTION,
-            ATTR_TARGET: self.entity_description.target,
-        }
-
-    async def async_update(self):
-        """Get the latest state of the binary sensor."""
-        if monitor := get_monitor_by_id(
-            self.coordinator.data.get("monitors", []), self.entity_description.key
-        ):
-            self._attr_is_on = monitor["status"] == 2
-
-
-def get_monitor_by_id(monitors, monitor_id):
-    """Return the monitor object matching the id."""
-    filtered = [monitor for monitor in monitors if monitor["id"] == monitor_id]
-    if len(filtered) == 0:
-        return
-    return filtered[0]
+    @property
+    def is_on(self) -> bool:
+        """Return True if the entity is on."""
+        return self.monitor_available
