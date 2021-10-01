e06ec3cd20e103e91cbc56b18223613cd0182230
errir <errir@outlook.jp>
2021-10-01 05:24:55 +0000

    Add long-term statistics support for rain sensors (#56847) home-assistant/core 7560c7b3de28b4cb7d567e3aa22f4a0fddd1d2a4

--- homeassistant/components/recorder/migration.py
+++ homeassistant/components/recorder/migration.py
@@ -24,7 +24,7 @@ from .models import (
     StatisticsShortTerm,
     process_timestamp,
 )
-from .statistics import _get_metadata, get_start_time
+from .statistics import get_metadata_with_session, get_start_time
 from .util import session_scope
 
 _LOGGER = logging.getLogger(__name__)
@@ -502,15 +502,6 @@ def _apply_update(instance, session, new_version, old_version):  # noqa: C901
                 ],
             )
     elif new_version == 21:
-        if engine.dialect.name in ["mysql", "oracle", "postgresql"]:
-            data_type = "DOUBLE PRECISION"
-        else:
-            data_type = "FLOAT"
-        _add_columns(
-            connection,
-            "statistics",
-            [f"sum_increase {data_type}"],
-        )
         # Try to change the character set of the statistic_meta table
         if engine.dialect.name == "mysql":
             for table in ("events", "states", "statistics_meta"):
@@ -522,9 +513,11 @@ def _apply_update(instance, session, new_version, old_version):  # noqa: C901
                 )
                 with contextlib.suppress(SQLAlchemyError):
                     connection.execute(
+                        # Using LOCK=EXCLUSIVE to prevent the database from corrupting
+                        # https://github.com/home-assistant/core/issues/56104
                         text(
                             f"ALTER TABLE {table} CONVERT TO "
-                            "CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+                            "CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci LOCK=EXCLUSIVE"
                         )
                     )
     elif new_version == 22:
@@ -571,10 +564,10 @@ def _apply_update(instance, session, new_version, old_version):  # noqa: C901
                     fake_start_time += timedelta(minutes=5)
 
         # Copy last hourly statistic to the newly created 5-minute statistics table
-        sum_statistics = _get_metadata(
+        sum_statistics = get_metadata_with_session(
             instance.hass, session, None, statistic_type="sum"
         )
-        for metadata_id in sum_statistics:
+        for metadata_id, _ in sum_statistics.values():
             last_statistic = (
                 session.query(Statistics)
                 .filter_by(metadata_id=metadata_id)
@@ -589,7 +582,6 @@ def _apply_update(instance, session, new_version, old_version):  # noqa: C901
                         last_reset=last_statistic.last_reset,
                         state=last_statistic.state,
                         sum=last_statistic.sum,
-                        sum_increase=last_statistic.sum_increase,
                     )
                 )
     else:
