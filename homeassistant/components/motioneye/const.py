835cdd1175cec1676a47c185349b17a8954a9321
errir <errir@outlook.jp>
2021-11-01 03:21:46 +0000

    Mobile app to update entity registry on re-register sensors (#58378) home-assistant/core a122cbab6135544a9bd42c44b4c3e062f66cf573

--- homeassistant/components/motioneye/const.py
+++ homeassistant/components/motioneye/const.py
@@ -28,6 +28,7 @@ DOMAIN: Final = "motioneye"
 ATTR_EVENT_TYPE: Final = "event_type"
 ATTR_WEBHOOK_ID: Final = "webhook_id"
 
+CONF_ACTION: Final = "action"
 CONF_CLIENT: Final = "client"
 CONF_COORDINATOR: Final = "coordinator"
 CONF_ADMIN_PASSWORD: Final = "admin_password"
@@ -79,11 +80,19 @@ EVENT_FILE_STORED_KEYS: Final = [
     KEY_WEB_HOOK_CS_MOTION_VERSION,
 ]
 
+EVENT_FILE_URL: Final = "file_url"
+EVENT_MEDIA_CONTENT_ID: Final = "media_content_id"
+
 MOTIONEYE_MANUFACTURER: Final = "motionEye"
+
+SERVICE_SET_TEXT_OVERLAY: Final = "set_text_overlay"
+SERVICE_ACTION: Final = "action"
+SERVICE_SNAPSHOT: Final = "snapshot"
 
 SIGNAL_CAMERA_ADD: Final = f"{DOMAIN}_camera_add_signal." "{}"
 SIGNAL_CAMERA_REMOVE: Final = f"{DOMAIN}_camera_remove_signal." "{}"
 
+TYPE_MOTIONEYE_ACTION_SENSOR = f"{DOMAIN}_action_sensor"
 TYPE_MOTIONEYE_MJPEG_CAMERA: Final = "motioneye_mjpeg_camera"
 TYPE_MOTIONEYE_SWITCH_BASE: Final = f"{DOMAIN}_switch"
 
